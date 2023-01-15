0e00100beda5ad851b8229c5f056f0751d90d810
Faizan-omer <omerfaizan1@gmail.com>
2023-01-15 18:24:19 +0500

    Added email sending functionality

--- src/components/Footer/ContactForm.jsx
+++ src/components/Footer/ContactForm.jsx
@@ -1,15 +1,31 @@
 import React from "react";
+import { sendForm } from "@emailjs/browser";
+import { PUBLIC_KEY, TEMPLATE_ID, SERVICE_ID } from "../../constants/email";
 
 const ContactForm = () => {
+  const form = React.useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY).then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      },
+    );
+  };
   return (
     <div className="flex flex-col justify-between">
-      <form action="" className="mt-3 w-full">
+      <form ref={form} onSubmit={sendEmail} className="mt-3 w-full">
         <div className="flex flex-col">
           <input
             type="text"
             id="name"
             className="bg-[#F3F3F3] w-full h-16 p-3 mt-2"
             placeholder="Name"
+            name="from_name"
           />
         </div>
         <div className="flex flex-col mt-3">
@@ -18,6 +34,7 @@ const ContactForm = () => {
             id="email"
             className="bg-[#F3F3F3] w-full h-16 p-3 mt-2"
             placeholder="Email"
+            name="email"
           />
         </div>
         <div className="flex flex-col mt-3">
@@ -26,6 +43,7 @@ const ContactForm = () => {
             id="name"
             className="bg-[#F3F3F3] w-full h-32 p-3 mt-2"
             placeholder="Type your message here"
+            name="message"
           />
         </div>
         <button type="submit" className="bg-[#2D2D2D] mt-3 w-52 h-16 text-white mt-8">
---
+++ src/constants/email.js
@@ -0,0 +1,5 @@
+module.exports = {
+  SERVICE_ID: "service_a8xkq66",
+  TEMPLATE_ID: "template_354y1bj",
+  PUBLIC_KEY: "PCaRGU4Pv-VdCzcae",
+};
