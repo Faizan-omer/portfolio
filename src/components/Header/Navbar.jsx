ad8deae3f202efa4fbca75fae644ce0b9b399644
Faizan-omer <omerfaizan1@gmail.com>
2023-01-15 15:35:24 +0500

    Download resume option

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -4,11 +4,22 @@ import Scroll from "react-scroll";
 import { contact, work } from "../../common/autoScroll";
 import { Link } from "react-router-dom";
 import { ABOUT, HOME } from "../../constants/routes";
+import resume from "../../assets/resume.pdf";
 
 import "./header.css";
 
 const Navbar = () => {
   const [expandNavBar, setExpandNavBar] = React.useState(false);
+
+  const downloadResume = () => {
+    const link = document.createElement("a");
+    link.href = resume;
+    link.download = "resume_faizan.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <>
       <div className="flex w-full px-5 md:px-9 py-6">
@@ -25,6 +36,12 @@ const Navbar = () => {
             <img src={menuButton} alt="" className="w-6 h-6" />
           </div>
           <ul className="hidden md:inline md:flex md:justify-between md:w-5/12">
+            <li
+              className="hover:cursor-pointer hover:underline self-center"
+              onClick={downloadResume}
+            >
+              Resume
+            </li>
             <li className="hover:cursor-pointer hover:underline self-center">
               <Link to={ABOUT}>About</Link>
             </li>
@@ -56,11 +73,17 @@ const Navbar = () => {
         </div>
       </div>
       {expandNavBar ? (
-        <ul className="md:hidden font-bold  text-lg">
-          <li className="self-center border-b-2 border-t-2 p-1 border-black">
-            <Link to={ABOUT}>About</Link>
+        <ul className="md:hidden font-bold bg-[#FCF4A3] text-lg">
+          <li
+            className="self-center text-center border-b border-t-2 p-1 border-black"
+            onClick={downloadResume}
+          >
+            Resume
           </li>
-          <li className="self-center border-b-2 -2 p-1 border-black">
+          <Link to={ABOUT}>
+            <li className="self-center text-center border-b p-1 border-black">About</li>
+          </Link>
+          <li className="self-center text-center border-b p-1 border-black">
             <Scroll.Link
               activeClass="active"
               to={work}
@@ -72,7 +95,7 @@ const Navbar = () => {
               Work
             </Scroll.Link>
           </li>
-          <li className="self-center border-b-2  p-1 border-black">
+          <li className="self-center text-center border-b p-1 border-black">
             <Scroll.Link
               activeClass="active"
               to={contact}
